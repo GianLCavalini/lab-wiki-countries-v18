8c02108a9e8cbaccfa4fdf53ceb6c233f3f5c558
Gian Lucca <giandacoll@gmail.com>
2022-04-18 09:11:04 -0300

    first commit

---
+++ src/Components/CountriesList/CountriesList.js
@@ -0,0 +1,15 @@
+import "../../countries.json"
+
+function CountriesList() {
+    const list = [..."../../countries.json"]
+    console.log(list);
+    return ( 
+
+            <ul>
+                <li>{}</li>
+            </ul>
+
+     );
+}
+
+export default CountriesList;
---
+++ src/Components/Navbar/NavBAr.js
@@ -0,0 +1,15 @@
+function NavBar() {
+    return ( 
+
+        <>
+            <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">LAB-WikiCountries</span>
+  </div>
+</nav>
+        </>
+
+     );
+}
+
+export default NavBar;
